ce14f54151f8dd89e2b06b4b96f98f0f510c9dd6
WEI_FANG LUO <kate860505@gmail.com>
2021-01-14 12:08:56 +0800

    add yt-header

--- main.js
+++ main.js
@@ -1,10 +1,42 @@
-const vm= Vue.createApp({
-    data(){
-        return{
+let app= Vue.createApp({});
 
-        };
-    },
-    methods:{
+app.component('yt-header',{
+    template:`
+    <div id="yt-header">
+        <div id="logo">
+            <img src="https://picsum.photos/seed/picsum/50/50" alt="">
+        </div>
+        <search-bar></search-bar>
+        <user-actions></user-actions>
+    </div>`,
+});
+app.component('yt-menu',{
+    template:`
+    <div id="yt-menu">
+        menu
+    </div>`,
+});
+app.component('yt-content',{
+    template:`
+    <div id="yt-content">
+        content
+    </div>`,
+});
 
-    },
-}).mount('#app')
+app.component('search-bar',{
+    template:`
+    <div id="search-bar">
+            <input type="text">
+            <button>search</button>
+    </div>`,
+});
+app.component('user-actions',{
+    template:`
+    <div id="user-actions">
+        <ul>
+            <li><a href="#">通知</a></li>
+            <li><a href="#">Profile</a></li>
+        </ul>     
+    </div>`,
+});
+app.mount('#app');
